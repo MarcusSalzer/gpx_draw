11cde2d11407cdd615de56711b7d7891647a8925
Marcus <marcus.salzer@hotmail.se>
2024-06-10 22:58:15 +0200

    polars?

--- experiments/file_formats.py
+++ experiments/file_formats.py
@@ -12,11 +12,10 @@ import polars as pl
 from utility import time_functions
 
 MAX_FILES = 10
+
 original_dir = "data/points_pandas"
 
 timings = {}
-
-
 
 
 def load_json_pd():
@@ -30,9 +29,28 @@ def load_json_pd():
     return all_acts, size
 
 
+def load_json_pl():
+    schema = {
+        "time": pl.Datetime,
+        "lat": pl.Float64,
+        "long": pl.Float64,
+        "speed_enh": pl.Float64,
+        "alt_enh": pl.Float64,
+        "hr": pl.Int64,
+    }
+    all_acts: dict[str, pd.DataFrame] = {}
+    size = 0
+    for filename in os.listdir(original_dir)[:MAX_FILES]:
+        fp = os.path.join(original_dir, filename)
+        df = pl.read_json(fp, schema=schema)  # TODO problems...
+        all_acts[filename.split(".")[0]] = df
+        size += os.path.getsize(fp)
+    return all_acts, size
+
+
 all_acts, original_size = load_json_pd()
 
-ACTS = all_acts.keys()
+ACTS = list(all_acts.keys())
 
 print(f"loaded original ({len(all_acts)} files)")
 print(f"original filesize {original_size/(1024*1024.0):.1f} MB")
@@ -58,21 +76,46 @@ def load_parq_pd():
     return all_acts, size
 
 
+def load_parq_pl():
+    all_acts = {}
+    size = 0
+    for k in ACTS:
+        fp = "data/points_parquet/" + k + ".parquet"
+        all_acts[k] = pl.read_parquet(fp)
+        size += os.path.getsize(fp)
+    return all_acts, size
+
+
 funs = [
     load_json_pd,
-    save_parq_pd,
+    load_json_pl,
     load_parq_pd,
+    load_parq_pl,
+    save_parq_pd,
 ]
 
 
 timings, outputs = time_functions(
     funs,
+    iterations=1,
+    format_time=True,
     table_outputs={"size (MB)": 1},
     out_format=lambda x: f"{x/(1024.0**2):.2f}",
-    verbose=True,
 )
 print(timings.to_markdown())
 
 
+print((outputs["load_parq_pd"][0][ACTS[0]].dtypes))
+# print("___")
+# print((outputs["load_parq_pl"][0][ACTS[0]].head()))
+print((outputs["load_json_pd"][0][ACTS[0]]))
+print("---")
+print((outputs["load_json_pl"][0][ACTS[0]]))
+
 for k in ACTS:
-    assert all_acts[k].equals(outputs["load_parq_pd"][0][k]), "Incorrect pandas_parquet"
+    assert outputs["load_json_pd"][0][k].equals(
+        outputs["load_parq_pd"][0][k]
+    ), "Incorrect pandas_parquet"
+
+
+print()
--- experiments/utility.py
+++ experiments/utility.py
@@ -1,25 +1,77 @@
 from timeit import default_timer
 import pandas as pd
+from statistics import mean, stdev
+import math
+
+
+def round_significant(value, digits=3):
+    """Round a float-like to a number of significant digits"""
+
+    if value == 0:
+        return 0
+    else:
+        scale = math.pow(10, digits - math.ceil(math.log10(abs(value))))
+
+        return round(value * scale) / scale
+
 
 def time_functions(
     functions,
-    table_outputs: dict = {"output": 0},
+    table_outputs: dict = None,
+    iterations=1,
+    format_time=False,
     out_format=None,
-    verbose=False,
+    verbose=True,
 ):
-    """Time a list of functions and provide timings and results"""
+    """Create a table of timing results for a list of functions.
+
+    ## Parameters
+    - functions: iterable of functions to test
+    - table_outputs (dict[str,int]): names and indicies of outputs to include in table
+    - iterations (int): number of times to measure. If >1, returns mean, std, max.
+    - format_time (bool): use only 3 significant digits
+    - out_format (function): applied to all outputs included in table
+    - verbose (bool): print progress
+
+    ## Returns
+    - results (DataFrame): table of timing results and selected function outputs
+    - outputs (dict): all outputs from all functions, indexed by function name.
+
+    """
+
+    if format_time:
+
+        def form_t(x):
+            return round_significant(x, 3)
+    else:
+
+        def form_t(x):
+            return x
+
     results = {}
     outputs = {}
     for f in functions:
         name = f.__name__
         if verbose:
             print(f"running {name}")
+
         t_start = default_timer()
         out = f()
-
         time = default_timer() - t_start
 
-        results[name] = {"time (s)": time}
+        if iterations <= 1:
+            results[name] = {"time (s)": form_t(time)}
+        else:
+            time = [time]
+            for _ in range(iterations):
+                t_start = default_timer()
+                out = f()
+                time.append(default_timer() - t_start)
+            results[name] = {
+                "mean(time)": form_t(mean(time)),
+                "std(time)": form_t(stdev(time)),
+                "max(time)": form_t(max(time)),
+            }
 
         if out:  # collect outputs
             outputs[name] = out
